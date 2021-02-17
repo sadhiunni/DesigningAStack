6d48d0d5b3bbd3a66a3de451941a06a6d4437283
sadhiunni <76890340+sadhiunni@users.noreply.github.com>
2021-02-17 19:25:57 +0530

    Add project files.

---
+++ DesigningAStack/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/* Designing a Stack which can do the following actions:
+ * Push an item
+ * Pop an item
+ * Display the items present in the stack
+ * Clear the stack
+ * Quit
+ */
+
+namespace DesigningAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            Stack stack = new Stack();
+
+            while (exit == false) 
+            {
+                Console.WriteLine("----------");
+                Console.WriteLine("The Stack");
+                Console.WriteLine("----------");
+                Console.WriteLine("Choose:\nPush\nPop\nDisplay\nClear\nQuit");
+                Console.WriteLine("----------");
+                var choice = Console.ReadLine();
+                
+                switch (choice.ToLower())
+                {
+                    case "push":
+                        Console.Clear();
+                        Console.WriteLine("Please enter the value you want to put in the stack");
+                        object value = (object)(Console.ReadLine());
+                        stack.push(value);
+                        break;
+                    case "pop":
+                        Console.Clear();
+                        stack.pop();
+                        break;
+                    case "display":
+                        Console.Clear();
+                        Console.WriteLine("The Stack");
+                        Console.WriteLine("----------");
+                        stack.Displaystack();
+                        Console.WriteLine("NOTE :Displaying the elements in the stack- first entered in the bottom of the stack and last in on the top of the stack");
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        stack.ClearStack();
+                        Console.WriteLine("Stack Cleared");
+                        break;
+                    case "quit":
+                        Console.Clear();
+                        Console.WriteLine("Exiting.....");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please make a valid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ DesigningAStack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesigningAStack
+{
+    class Stack 
+    {
+        private List<object> _stackValues = new List<object>();
+
+        //push
+        public void push(object value)
+        { 
+            if ( value == null  )
+            {
+                throw new InvalidOperationException("Please enter a value!");
+            }
+            _stackValues.Add(value);
+        }
+
+        //pop
+        public void pop() 
+        {
+            int LastValue = _stackValues.Count - 1;
+            if (_stackValues == null || LastValue==-1) 
+            {
+                throw new InvalidOperationException("Stack Empty");
+            }
+            Console.WriteLine("The value popped : "+ _stackValues[LastValue]);
+            _stackValues.RemoveAt(LastValue);
+        }
+
+        //Display
+        public void Displaystack() 
+        {
+            for (int i = _stackValues.Count-1; i >= 0; i--)
+            {
+                Console.WriteLine(_stackValues[i]);
+            }
+        }
+
+        //Clear
+        public void ClearStack()
+        {
+            _stackValues.Clear();
+        }
+    }
+}
